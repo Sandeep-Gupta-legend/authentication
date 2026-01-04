ad19816fdccecb0e6431378ddd0a9bd422dca8d7
Sandeep-Gupta-legend <sandeep1legend@gmail.com>
2026-01-04 10:41:45 +0530

    intial commit

---
+++ auth-frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Login from '../pages/Login'
+import Register from '../pages/Register'
+import ForgotPassword from '../pages/ForgotPassword'
+
+
+const App = () => {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Login/>}/>
+      <Route path="/register" element={<Register/>}/>
+      <Route path="/forgot-password" element={<ForgotPassword/>}/>
+      
+
+    </Routes>
+    </BrowserRouter>
+    
+  )
+}
+
+export default App
